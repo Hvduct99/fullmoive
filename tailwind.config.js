50e32fdeb3af9e2045176bae25cb186bd7fe78c2
hvduct <17january2k@gmail.com>
2026-02-12 21:27:10 +0700

    Fix Tailwind CSS configuration for animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,25 @@ module.exports = {
         secondary: '#e50914', // Netflix Red accent
         text: '#eeb'
       },
+      keyframes: {
+        fadeInDown: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeIn: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fadeInDown 0.3s ease-out',
+        'fade-in-up': 'fadeInUp 0.5s ease-out',
+        'fade-in': 'fadeIn 0.5s ease-out',
+      },
     },
   },
   plugins: [],
